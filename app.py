86b595645ef391691e513953fc7f913eb657e192
James Hogan <jameshoganobair@gmail.com>
2021-01-28 20:21:21 +0100

    added sequence matching

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 import json
+import difflib
+from difflib import SequenceMatcher
 
 data = {}
 
@@ -7,22 +9,37 @@ with open('data.json') as d:
 
 # print(data['success'])
 
+def check(word):
+    for x in data:
+        if SequenceMatcher(None, word, x).ratio() > .85:
+            ans = input(f"Did you mean {x}?\n").lower()
+            if ans == 'yes' or ans == 'y':
+                return x
+            if ans == 'no' or ans == 'n' :
+                return 'none'
+            
+
+
 def translate(word):
     word = word.lower()
     index = 0
     if word not in data:
-        print('No definition available')
+        correct = check(word)
+        if correct == 'none':
+            print('No definition available')
+        else :
+            translate(correct)
     # print(data[word])
-    if(word.lower() != 'exit' and word in data):
+    if(word.lower() != '\exit' and word in data):
         for item in data[word]:
             index += 1
             print(f"{index}: {item}\n")
 
 word = ''
 
-while word != 'exit':
+while word != '\exit':
     word = input('Select a word or enter "exit" to exit\n')
     translate(word)
 
-if word == 'exit':
+if word == '\exit':
     print('Goodbye')
