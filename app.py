3cc8bd61bf498f2c4e037b276632d5dbe9b466b6
James Hogan <jameshoganobair@gmail.com>
2021-01-28 19:43:25 +0100

    adj for cases and err cases

--- app.py
+++ app.py
@@ -8,7 +8,15 @@ with open('data.json') as d:
 # print(data['success'])
 
 def translate(word):
-    print(data[word])
+    word = word.lower()
+    index = 0
+    if word not in data:
+        print('No definition available')
+    # print(data[word])
+    if(word.lower() != 'exit' and word in data):
+        for item in data[word]:
+            index += 1
+            print(f"{index}: {item}\n")
 
 word = ''
 
