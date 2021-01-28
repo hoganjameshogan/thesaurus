3e2f3d3c882c6325d7e560d1d0529e2c610212f2
James Hogan <jameshoganobair@gmail.com>
2021-01-28 19:12:37 +0100

    major groundbreaking changes

--- app.py
+++ app.py
@@ -5,4 +5,16 @@ data = {}
 with open('data.json') as d:
     data = json.load(d)
 
-print(data['success'])
+# print(data['success'])
+
+def translate(word):
+    print(data[word])
+
+word = ''
+
+while word != 'exit':
+    word = input('Select a word or enter "exit" to exit\n')
+    translate(word)
+
+if word == 'exit':
+    print('Goodbye')
